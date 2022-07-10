c7a4ab2cec9943d131a0fcaaeed18be01a4b5b78
Sara Prager <sara.prager@algosec.com>
2022-07-10 13:05:44 +0300

    Sample sidebar app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'sidebar-poc';
+  isMobile: boolean = false;
+  isLoggedIn: boolean = true;
+  sidenavIsOpen: boolean = false;
+  user: any = undefined;
+  copyrightText: string = '© copyright text here...'
+
+  @ViewChild('drawer') drawer : any;
+  @ViewChild('sidenav') sidenav: any;
+
+  headerIcons: { name: string, url?: string }[] = [
+    { name: 'alarm', url: '/'},
+    { name: 'help', url: '/'},
+    { name: 'settings', url: '/'},
+    { name: 'logout' },
+  ];
+
+  sidebarLinks: { label: string, url: string, icon: string }[] = [
+    { label: 'Dashboard', url: 'dashboard', icon: 'dashboard'},
+    { label: 'Devices', url: 'devices', icon: 'devices'},
+    { label: 'Users', url: 'users', icon: 'people'},
+    { label: 'Event Log', url: 'event-log', icon: 'event'},
+    { label: 'Accounting Log', url: 'accounting-log', icon: 'history'},
+  ];
+
+  constructor() {
+  }
+
+  onLogout() {
+  }
+
+  // Desktop menu toggle
+  onToggleDrawer() {
+    if (this.drawer) {
+      this.drawer.toggle();
+    }
+  }
+
+  // Mobile menu toggle
+  onToggleSidenav() {
+    if (this.sidenav) {
+      this.sidenav.toggle();
+      this.sidenavIsOpen = !this.sidenavIsOpen;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
